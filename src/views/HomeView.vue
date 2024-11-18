adb6bbc5524926d1092c0e8caae9b13ad38c8a29
trinhmanhcuong2001 <cuong69pro@gmail.com>
2024-11-18 15:23:50 +0700

    b2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useStore } from "vuex";
+import { computed, ref } from "vue";
+
+const store = useStore();
+
+const count = computed(() => store.state.count);
+
+const increment = () => {
+    store.commit("increment");
+};
+</script>
 
 <template>
-    <main></main>
+    <main>
+        <h1>Đây là trang cộng</h1>
+        <h4>Số: {{ count }}</h4>
+        <button @click="increment">Cộng</button>
+    </main>
 </template>
